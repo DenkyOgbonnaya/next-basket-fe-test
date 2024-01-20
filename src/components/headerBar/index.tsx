b7e36aaa6d466f354a4f2725a4a6637a9ff6ce3c
DenkyOgbonnaya <denkyogb@gmail.com>
2024-01-20 22:20:12 +0100

    chore:update items alignment

--- src/components/headerBar/index.tsx
+++ src/components/headerBar/index.tsx
@@ -17,83 +17,80 @@ export default function HeaderBar() {
       height="3.625rem"
       display={{ mobile: "none", laptop: "flex" }}
       alignItems="center"
+      justifyContent="space-between"
+      px={{mobile: "2.19rem", laptop:"2.38rem"}}
     >
-      <Container maxWidth="desktop" >
-        <Box display="flex" justifyContent="space-between">
-          <Stack direction="row" gap="0.62rem">
-            <Stack
-              direction="row"
-              useFlexGap
-              gap="0.31rem"
-              alignItems="center"
-              paddingX="0.62rem"
-            >
-              <PhoneIcon />
-              <Link
-                href=""
-                variant="text-xs"
-                letterSpacing="0.0125rem"
-                lineHeight="1rem"
-                color="light.main"
-                fontWeight="400"
-                sx={{ textDecoration: "none" }}
-              >
-                (225) 555-0118
-              </Link>
-            </Stack>
-
-            <Stack direction="row" useFlexGap gap="0.31rem" alignItems="center">
-              <EmailIcon />
-              <Link
-                variant="text-xs"
-                letterSpacing="0.0125rem"
-                lineHeight="1rem"
-                color="light.main"
-                fontWeight="400"
-                href="#"
-                sx={{ textDecoration: "none" }}
-              >
-                michelle.rivera@example.com
-              </Link>
-            </Stack>
-          </Stack>
-          <Typography
+      <Stack direction="row" gap="1.88rem">
+        <Stack
+          direction="row"
+          useFlexGap
+          gap="0.31rem"
+          alignItems="center"
+        >
+          <PhoneIcon />
+          <Link
+            href=""
             variant="text-sm"
-            component="span"
             letterSpacing="0.0125rem"
             lineHeight="1.5rem"
             color="light.main"
             fontWeight="700"
+            sx={{ textDecoration: "none" }}
           >
-            Follow Us and get a chance to win 80% off
-          </Typography>
+            (225) 555-0118
+          </Link>
+        </Stack>
 
-          <Stack direction="row" useFlexGap gap="1rem" alignItems="center">
-            <Typography
-              variant="text-sm"
-              letterSpacing="0.0125rem"
-              lineHeight="1.5rem"
-              color="light.main"
-              fontWeight="700"
-              component="span"
-            >
-              Follow us :
-            </Typography>
-            <Link href="#">
-              <InstagramIcon />
-            </Link>
-            <Link href="#">
-              <YoutubeIcon />
-            </Link>
-            <Link href="#">
-              <FacebookIcon />
-            </Link>
-            <Link href="#">
-              <TwitterIcon />
-            </Link>
-          </Stack>
-        </Box>
-      </Container>
+        <Stack direction="row" useFlexGap gap="0.31rem" alignItems="center">
+          <EmailIcon />
+          <Link
+            variant="text-sm"
+            letterSpacing="0.0125rem"
+            lineHeight="1.5rem"
+            color="light.main"
+            fontWeight="700"
+            href="#"
+            sx={{ textDecoration: "none" }}
+          >
+            michelle.rivera@example.com
+          </Link>
+        </Stack>
+      </Stack>
+      <Typography
+        variant="text-sm"
+        component="span"
+        letterSpacing="0.0125rem"
+        lineHeight="1.5rem"
+        color="light.main"
+        fontWeight="700"
+      >
+        Follow Us and get a chance to win 80% off
+      </Typography>
+
+      <Stack direction="row" useFlexGap gap="1rem" alignItems="center">
+        <Typography
+          variant="text-sm"
+          letterSpacing="0.0125rem"
+          lineHeight="1.5rem"
+          color="light.main"
+          fontWeight="700"
+          component="span"
+        >
+          Follow us :
+        </Typography>
+        <Link href="#">
+          <InstagramIcon />
+        </Link>
+        <Link href="#">
+          <YoutubeIcon />
+        </Link>
+        <Link href="#">
+          <FacebookIcon />
+        </Link>
+        <Link href="#">
+          <TwitterIcon />
+        </Link>
+      </Stack>
     </Box>
   );
 }
--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -9,7 +9,7 @@ import { ChevronDownIcon, MenuIcon, UserIcon } from "@/assets";
 import SearchBar from "../searchBar";
 import CartIndicator from "../cartIndicator";
 import SavedIndicator from "../savedIndicator";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
   const [menuExpanded, setMenuExpanded] = useState(false);
@@ -19,7 +19,7 @@ export default function NavBar() {
   const pathName = usePathname();
 
   const activeLink = (url: string, pathname: string) => {
-    return pathname === url 
+    return pathname === url;
   };
 
   const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
@@ -52,17 +52,24 @@ export default function NavBar() {
     },
   ];
   return (
-    <Box sx={{ flexGrow: 1 }} position="relative" mt="0.75rem">
+    <Box sx={{ flexGrow: 1 }} position="relative">
       <AppBar
         position="static"
         sx={{
           bgcolor: "background.default",
           boxShadow: "none",
           height: "3.625rem",
+          px:{mobile: "2.19rem", laptop:"2.38rem"}
         }}
       >
-        <Container maxWidth="desktop">
-          <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Toolbar sx={{ justifyContent: "space-between", px:"0px" }}>
+          <Stack
+            useFlexGap
+            direction="row"
+            gap="calc(4.94rem + 2.5rem)"
+            alignItems="center"
+            px="0px"
+          >
             <Typography
               variant="text-2xl"
               component="span"
@@ -71,7 +78,6 @@ export default function NavBar() {
               color="text.primary"
               fontWeight="700"
               noWrap
-              //   sx={{ display: { mobile: "none", laptop: "block" } }}
             >
               Bandage
             </Typography>
@@ -171,65 +177,64 @@ export default function NavBar() {
                 Pages
               </Link>
             </Stack>
+          </Stack>
+
+          <Stack
+            component="nav"
+            direction="row"
+            useFlexGap
+            gap="1.88rem"
+            alignItems="center"
+          >
             <Stack
-              component="nav"
               direction="row"
               useFlexGap
-              gap="1.88rem"
+              gap="0.31rem"
               alignItems="center"
+              display={{ mobile: "none", tablet: "flex" }}
             >
-              <Stack
-                direction="row"
-                useFlexGap
-                gap="0.31rem"
-                alignItems="center"
-                display={{ mobile: "none", tablet: "flex" }}
+              <UserIcon />
+              <Link
+                href=""
+                variant="text-sm"
+                letterSpacing="0.0125rem"
+                lineHeight="1.5rem"
+                color="primary"
+                fontWeight="700"
+                sx={{ textDecoration: "none" }}
               >
-                <UserIcon />
-                <Link
-                  href=""
-                  variant="text-sm"
-                  letterSpacing="0.0125rem"
-                  lineHeight="1.5rem"
-                  color="primary"
-                  fontWeight="700"
-                  sx={{ textDecoration: "none" }}
-                >
-                  Login / Register
-                </Link>
-              </Stack>
-
-              <SearchBar />
-              <CartIndicator />
-              <Box display={{ mobile: "none", laptop: "block" }}>
-                <SavedIndicator />
-              </Box>
-              <Box
-                component="button"
-                onClick={toggleMenu}
-                display={{ mobile: "block", laptop: "none" }}
-                sx={{
-                  unset: "all",
-                  border: "none",
-                  backgroundColor: "transparent",
-                  outline: "none",
-                  padding: "0px",
-                }}
-              >
-                <MenuIcon />
-              </Box>
+                Login / Register
+              </Link>
             </Stack>
-          </Toolbar>
-        </Container>
+
+            <SearchBar />
+            <CartIndicator />
+            <Box display={{ mobile: "none", laptop: "block" }}>
+              <SavedIndicator />
+            </Box>
+            <Box
+              component="button"
+              onClick={toggleMenu}
+              display={{ mobile: "block", laptop: "none" }}
+              sx={{
+                unset: "all",
+                border: "none",
+                backgroundColor: "transparent",
+                outline: "none",
+                padding: "0px",
+              }}
+            >
+              <MenuIcon />
+            </Box>
+          </Stack>
+        </Toolbar>
       </AppBar>
 
-      {
-          /* 
+      {/* 
 
           MOBILE MENU
 
-          */
-      }
+          */}
       <Stack
         useFlexGap
         direction="column"
@@ -238,7 +243,7 @@ export default function NavBar() {
         display={{ mobile: "flex", laptop: "none" }}
         alignItems="center"
         position="absolute"
-        bgcolor='background.paper'
+        bgcolor="background.paper"
         top={menuExpanded ? 150 : -500}
         margin="0 auto"
         right="50%"
